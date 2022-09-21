812e525e6d10f7e76057266e05fa0d7226c145a8
VMishutkin <vladmish@list.ru>
2022-09-21 18:44:48 +0700

    Сделал Класс, сделал тесты

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,28 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        StringListImpl stringList = new StringListImpl(5);
+        stringList.add("Hello!");
+        stringList.add("HI!");
+        System.out.println(stringList.size());
+        System.out.println(stringList);
+        System.out.println(stringList);
+        stringList.remove("HI!");
+        System.out.println(stringList);
+        stringList.add(1, "Second String");
+        System.out.println(stringList);
+         stringList.add("How are you doing?");
+        stringList.add("WHATSAAAAAP");
+        stringList.remove(2);
+        System.out.println(stringList);
+        System.out.println(stringList.size());
+
+        StringList otherList = new StringListImpl(7);
+        otherList.add("Hello!");
+        otherList.add(1, "Second String");
+        System.out.println(stringList);
+        System.out.println(otherList);
+        System.out.println(stringList.equals(otherList));
+
     }
 }
